2ab7cf3d63a13f2e94ce6ea073c98f409948e629
preetammishra08-spec <preetam.mishra08@gmail.com>
2026-09-20 14:39:03 +0530

    Create Multiplication.c

---
+++ Lab_02/Multiplication.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+int a;
+int b;
+int c;
+
+printf("Enter first number:");
+scanf("%d",&a);
+
+printf("Enter second number:");
+scanf("%d",&b);
+
+c = a*b;
+
+printf("addition,%d",c);
+
+  return 0;
+}  
