ff884e9b708b195ab90c98e1452e85c201a0137a
preetammishra08-spec <preetam.mishra08@gmail.com>
2026-09-26 19:48:05 +0530

    Create Signed_Unsigned.c

---
+++ Lab_4/Signed_Unsigned.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    signed int a = -30;
+    unsigned int b = 20;
+
+    printf("Signed value = %d\n", a);
+    printf("Unsigned value = %u\n", b);
+
+    return 0;
+}
